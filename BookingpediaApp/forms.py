df983a987481b71e680894d96bc3e4f5493bef95
jakobslason <jakobslason@gmail.com>
2022-11-01 08:08:13 -0400

    Final Changes

--- BookingpediaApp/forms.py
+++ BookingpediaApp/forms.py
@@ -1,35 +1,35 @@
 from django import forms
-from .models import Customer, Room, Hotel, Reservation, Item, Transaction
+from .models import Customer, Room, Hotel, Item
 
 
 class CustomerEditForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', max_length=100)
-    bill = forms.FloatField(label='Bill')
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Username', max_length=100)
+    password = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Password', max_length=100)
+    bill = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control'}), label='Bill')
 
 class HotelEditForm(forms.Form):
-    name = forms.CharField(label='Hotel Name', max_length=100)
-    address = forms.CharField(label='Address', max_length=100)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Hotel Name', max_length=100)
+    address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Address', max_length=100)
 
 class RoomEditForm(forms.Form):
-    number = forms.IntegerField(label='Room Number')
-    type = forms.CharField(label='Room Type', max_length=100)
-    price = forms.FloatField(label='Room Price')
-    hotel = forms.ModelChoiceField(queryset = Hotel.objects.all())
+    number = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}), label='Room Number')
+    type = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Room Type', max_length=100)
+    price = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control'}), label='Room Price')
+    hotel = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), queryset = Hotel.objects.all())
 
 class ReservationEditForm(forms.Form):
-    start_date = forms.DateField(label='Start Date')
-    end_date = forms.DateField(label='End Date')
-    room = forms.ModelChoiceField(queryset = Room.objects.all())
-    customer = forms.ModelChoiceField(queryset = Customer.objects.all())
+    start_date = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control'}), label='Start Date')
+    end_date = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control'}), label='End Date')
+    room = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), queryset = Room.objects.all())
+    customer = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), queryset = Customer.objects.all())
 
 class ItemEditForm(forms.Form):
-    name = forms.CharField(label='name', max_length=100)
-    price = forms.FloatField(label='price')
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='name', max_length=100)
+    price = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control'}), label='price')
 
 class TransactionEditForm(forms.Form):
-    timestamp = forms.DateTimeField(label='Purchased At')
-    count = forms.FloatField(label='Number Purchased')
-    item = forms.ModelChoiceField(queryset = Item.objects.all())
-    room = forms.ModelChoiceField(queryset = Room.objects.all())
-    customer = forms.ModelChoiceField(queryset = Customer.objects.all())
+    timestamp = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'form-control'}), label='Purchased At')
+    count = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control'}), label='Number Purchased')
+    item = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), queryset = Item.objects.all())
+    room = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), queryset = Room.objects.all())
+    customer = forms.ModelChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), queryset = Customer.objects.all())
