34b9cde5bbc108af74e19e3e43195970df76e9df
jakobslason <jakobslason@gmail.com>
2022-10-31 20:17:41 -0400

    admin page

--- BookingpediaApp/admin.py
+++ BookingpediaApp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Customer
+from .models import Customer, Room, Hotel, Reservation, Item, Transaction
 
+admin.site.register(Customer)
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Reservation)
+admin.site.register(Item)
+admin.site.register(Transaction)
